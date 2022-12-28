508f70130b2d3195f5fc24b006746b119f8ebd07
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2022-12-28 19:57:43 +0400

    uncommended

--- ifstatements.py
+++ ifstatements.py
@@ -1,29 +1,29 @@
-#x = 3
-#if x < 10:
-#    print('x below ten')
+x = 3
+if x < 10:
+    print('x below ten')
 
-#y = 11
-#if y > 10:
-#    print('y is greater than ten')
-#z = 2
-#if z > 1 and z < 4:
-#    print('z is in range')
+y = 11
+if y > 10:
+    print('y is greater than ten')
+z = 2
+if z > 1 and z < 4:
+    print('z is in range')
 
-#gender = input("Your Gender? ")
-#gender = gender.lower()
+gender = input("Your Gender? ")
+gender = gender.lower()
 
-#if gender == "male":
-#    print("Your cat is male")
-#elif gender == "female":
-#    print("Your cat is female")
-#else:
-#    print("Invalid input")
+if gender == "male":
+    print("Your cat is male")
+elif gender == "female":
+    print("Your cat is female")
+else:
+    print("Invalid input")
 
-#    age = int(input("Age of your cat? "))
-#    if age < 5:
-#        print("Your cat is young.")
-#    else:
-#        print("Your cat is adult.")
+    age = int(input("Age of your cat? "))
+    if age < 5:
+        print("Your cat is young.")
+    else:
+        print("Your cat is adult.")
 
 # password = string(input("please write down your password"))
 
