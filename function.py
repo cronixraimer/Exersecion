74fba60e48da87b5b3a71b65b69cecc9cec4a61c
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-03 17:35:34 +0400

    funtion sample

---
+++ function.py
@@ -0,0 +1,21 @@
+def currentYear():
+    print('2023')
+
+currentYear()
+#summation function
+def f(x, y):
+    return x*y
+
+print(f(3,4))
+
+#return variables
+result = f(3,4)
+print(result)
+
+def sumlist():
+    mylist = [1, 2, 3, 4,5 ]
+    for x in mylist:
+        x = x + x
+        print(x)
+
+sumlist()
