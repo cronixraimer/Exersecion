92a5cbf70069f5a5df8b8dc8d2a85076a9e35a85
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-03 11:44:10 +0400

    IndefiniteIteration

---
+++ IndefiniteIteration.py
@@ -0,0 +1,12 @@
+x = 3
+
+while x < 10:
+    print(x)
+    x = x + 1
+
+clist = ["Canda", "USA", "Mexico"]
+index = 0
+while (index < 3):
+    for x in clist:
+        print(x)
+        index = index + 1
