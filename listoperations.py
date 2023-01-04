e034fa7e67268825584ab6ae0c1285d5b210d80f
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-04 13:33:34 +0400

    listoperation

---
+++ listoperations.py
@@ -0,0 +1,17 @@
+x = [3, 4, 5]
+print(x)
+x.append(6) #add an element to the list on the last position
+print(x)
+x.append(7) #add an element to the list on the last position
+print(x)
+x.pop() #remove an element from last of list
+print(x)
+print(x[0]) #access elements
+print(x[-1]) #access elements
+
+y = [6, 4, 2]
+y[1] = '3'
+x = [12, 8, 4]
+z = [ ]
+z.append(y + x)
+print(z)
