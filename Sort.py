f8ec9781985d821429ad736dc297ece9cb3194ee
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-05 11:46:18 +0400

    sortlist

---
+++ Sort.py
@@ -0,0 +1,18 @@
+x = [3, 5, 41, 98, 1, 66, 0 , 69]
+x.sort()
+print(x)
+words = ["Be", "Nice", "Always", "Door", "Eat"]
+words.sort()
+words = words[::-1]
+print(words)
+y = [3, 6, 41, 21, 69, 72, 1, 0]
+y.sort()
+y = y[::-1]
+print(y)
+
+#list with pairs, sort on the first element
+z = [ (3,6),(4,7),(5,9),(8,4),(3,1)]
+z.sort()
+print(z)
+z[0] = z.sort()
+print(z)
