262db6a523e6b5f78c4cfe4919cd77842ab23586
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-06 12:11:04 +0400

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,21 @@
+words = {}
+
+words ["TBH"] = "To Be Honest"
+words ["BTW"] = "By The Way"
+words ["BRB"] = "Be Right Back"
+
+print(words ["TBH"])
+print(words ["BRB"])
+
+#mapping from countries to country short codes
+
+countries = {}
+
+countries ["US"] = "United States of America"
+countries ["UK"] = "United Kingdom"
+countries ["RU"] = "Russia"
+countries ["BR"] = "Brazil"
+
+for i in countries:
+    print(i, '->', countries[i])
+#:)
