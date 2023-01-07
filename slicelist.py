7f7ec11debe3e6066028b9dd404a545159362e67
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-01-07 19:18:07 +0400

    slicelist

---
+++ slicelist.py
@@ -0,0 +1,16 @@
+persons = [ "John", "Marissa", "Pete", "Dayton" ]
+
+slice = persons[0:2]
+print(slice)
+
+destination = "summer holiday at beach"
+mySlice = destination[0:6]
+print(mySlice)
+
+pizzas = ["Hawai","Pepperoni","Fromaggi","Napolitana","Diavoli"]
+mypizzas = pizzas[0:4]
+print(mypizzas)
+
+h = "Hello World"
+d = h[5:11 ]
+print(d)
