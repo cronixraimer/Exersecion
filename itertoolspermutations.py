c7dd67a11b4542338f602185b1a0efe9ccdb019f
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-06-29 00:28:39 +0400

    HackerRank itertools permutations task

---
+++ itertoolspermutations.py
@@ -0,0 +1,17 @@
+#read otertools.permutation library then solve problem 
+def lexicographic(s, n):
+    a = sorted(s)
+
+    for i in range(n):
+
+
+
+first_multiple_input = input()
+
+s = first_multiple_input[0]
+
+n = int(first_multiple_input[1])
+
+result = lexicographic(s, n)
+
+print(result)
