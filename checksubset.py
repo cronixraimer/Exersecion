4fc859253b3a3567b5349e59f64d85ef96b40344
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-10-11 09:17:52 +0400

    checksubset task

---
+++ checksubset.py
@@ -0,0 +1,10 @@
+
+n = int(input())
+m = 2 * n
+for i in range(m):
+    a_1 = int(input())
+    for j in range(a_1):
+        a = set(map(int, input().split()))
+    b_1 = int(input())
+    for k in range(b_1):
+        b = set(map(int, input().split())
