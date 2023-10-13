81373b3b1d8e6c2b36c279814923ed740fc7bfdc
Cronix Raimer <ayubovbakhtiyor@gmail.com>
2023-10-13 11:31:45 +0400

    map and lambda fibonacci task done hackerranlk

---
+++ mapandlambdafibonacci.py
@@ -0,0 +1,19 @@
+cube = lambda x: x**3 # complete the lambda function
+
+def fibonacci(n):
+    # return a list of fibonacci numbers
+    if n == 0:
+        fib = []
+    elif n == 1:
+        fib = [0]
+    elif n > 1:
+        fib = [0, 1]
+        for i in range(2, n):
+            fib.append(fib[i - 1] + fib[i - 2])
+    else:
+        pass
+    return fib
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
